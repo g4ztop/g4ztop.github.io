b8c3f1993fa10eba90221add454a796e238f92ff
Mitchell Craft <mitchellcraft@Mitchells-MacBook-Air.local>
2025-07-04 15:27:02 -0400

    Enhance hover effect to make images fill frame edges

--- src/App.tsx
+++ src/App.tsx
@@ -166,7 +166,7 @@ function Home() {
               <img
                 src={project.image}
                 alt={project.title}
-                className="w-full h-auto object-cover transition-all duration-300 group-hover:scale-105 rounded-lg"
+                className="w-full h-auto object-cover transition-all duration-300 group-hover:scale-150 rounded-lg"
               />
               
               {/* Hover Overlay */}
